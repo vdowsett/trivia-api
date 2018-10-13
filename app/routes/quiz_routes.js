0b84d9e81373a0a899b756f33078823225d16410
Vicky Dowsett <vdowsett@gmail.com>
2018-10-13 10:52:15 -0500

    trivia api part 4

--- app/routes/quiz_routes.js
+++ app/routes/quiz_routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var apiRouter = express.Router();
 var sampleQuiz = require('./../quiz_fixture.js');
+var quiz = require('./../quiz.js');
 
 apiRouter.get('/', function(req, res) {
   res.json({ message: 'hooray! your API is working' });
@@ -9,6 +10,24 @@ apiRouter.get('/', function(req, res) {
 
 apiRouter.get('/sample', function(req, res) {
   res.json(sampleQuiz);
+});
+
+apiRouter.get('/category/:category', function(req, res) {
+  quiz
+    .getQuiz(req.params)
+    .then(function(result) { res.json (result); });
+});
+
+apiRouter.get('/category/:category/difficulty/:difficulty', function(req, res) {
+  quiz
+    .getQuiz(req.params)
+    .then( function(result) {res.json (result); });
+});
+
+apiRouter.get('/category/:category/difficulty/:difficulty/count/:count', function(req, res) {
+ quiz
+   .getQuiz(req.params)
+   .then(function(result){res.json(result);});
 });
 
 module.exports = apiRouter;
